192a5407fa6a7c3825e3511a645ecd50681c0f6c
Crowdbotics <team@crowdbotics.com>
2021-08-17 18:11:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "prettylittlecore_29887Identifier",
+  url: "https://prettylittlecore_29887Identifier.botics.co",
+  api: "https://prettylittlecore_29887Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
